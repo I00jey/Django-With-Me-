87e5555481ba9a6641196d3591178663ee0394d6
I00jey <cyj5056@gmail.com>
2025-02-23 17:53:17 +0900

    게시판 페이지 만들기 1

--- practice/main/admin.py
+++ practice/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+# 관리자가 게시글에 접근할 권한 부여
+admin.site.register(Post)
--- practice/main/models.py
+++ practice/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# 모델 => 붕어빵을 만드는 틀
+# 게시글 Post
+# 제목 postname
+# 내용 contents
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
